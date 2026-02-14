f1c0e0ad9348dd6559736f5ae3c964d861748735
Luis Aaron Aviles Robles <eresuntomatito@MacBookPro.fritz.box>
2026-02-14 22:23:01 +0100

    Fix TypeScript build errors for deployment
    - Allow JS files in tsconfig
    - Remove unused React import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserProgressProvider } from './context/UserProgressContext';
 import AppLayout from './components/AppLayout';
